b3002dd56936ceae176c0bcc1f37f76fca1e7de5
MaheshMatta999 <maheshmatta999@gmail.com>
2025-06-23 00:40:15 +0530

    Little Lemon Booking Website capstone project

---
+++ src/components/pages/Home/index.js
@@ -0,0 +1,17 @@
+import Hero from './Hero';
+import WeekSpecials from './WeekSpecials';
+import RaveReviews from './RaveReviews';
+
+
+
+const Home = () => {
+  return (
+    <>
+      <Hero />
+      <WeekSpecials />
+      <RaveReviews />
+    </>
+  );
+};
+
+export default Home;
